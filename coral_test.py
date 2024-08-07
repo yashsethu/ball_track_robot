af46e373f55137200aa3b9c46f0e4c500f43bd82
Yash Sethuraman <yashsethu@gmail.com>
2024-08-06 21:58:41 -0700

    Bunch ofa changes a

--- coral_test.py
+++ coral_test.py
@@ -2,14 +2,10 @@ import cv2
 import numpy as np
 from pycoral.adapters import classify
 from pycoral.utils.edgetpu import make_interpreter
-from tflite_runtime.interpreter import Interpreter
 
-# Import the required libraries
 # Load the model
 model_path = "path/to/your/model.tflite"
-interpreter = Interpreter(model_path)
-
-# Allocate tensors
+interpreter = make_interpreter(model_path)
 interpreter.allocate_tensors()
 
 # Get input and output details
@@ -19,15 +15,6 @@ output_details = interpreter.get_output_details()
 # Get input and output shapes
 input_shape = input_details[0]["shape"]
 output_shape = output_details[0]["shape"]
-
-# Perform inference
-input_data = ...  # Provide input data for inference
-interpreter.set_tensor(input_details[0]["index"], input_data)
-interpreter.invoke()
-output_data = interpreter.get_tensor(output_details[0]["index"])
-
-# Release resources
-interpreter.close()
 
 # Open the webcam
 cap = cv2.VideoCapture(0)
@@ -46,11 +33,11 @@ while True:
 
     # Preprocess the frame
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    input_tensor = cv2.resize(frame_rgb, (224, 224))
+    input_tensor = cv2.resize(frame_rgb, (input_shape[2], input_shape[1]))
     input_tensor = np.expand_dims(input_tensor, axis=0)
 
     # Run the model
-    interpreter.set_tensor(interpreter.get_input_details()[0]["index"], input_tensor)
+    interpreter.set_tensor(input_details[0]["index"], input_tensor)
     interpreter.invoke()
 
     # Get the results
@@ -67,6 +54,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-# When everything done, release the capture
+# When everything is done, release the capture
 cap.release()
 cv2.destroyAllWindows()
