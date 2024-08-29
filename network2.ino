cb92ded5f1084f2c0a5f8707e1180b6d3ed380d6
Yash Sethuraman <yashsethu@gmail.com>
2024-08-29 00:20:28 -0700

    Create network2.ino

---
+++ network2.ino
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <U8g2lib.h>
+#include <WiFi.h>
+
+const int x = 5;
+const int y = 13;
+
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C
+u8g2(U8G2_R0, 22, 21, U8X8_PIN_NONE);
+
+void print(int x, int y, const char *str)
+{
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_profont10_mr);
+    u8g2.drawStr(x, y, str);
+    u8g2.sendBuffer();
+}
+
+void setup()
+{
+    u8g2.begin();
+
+    WiFi.mode(WIFI_STA); // Optional
+    WiFi.disconnect();
+
+    print(x, y, "Setup Done");
+    delay(1000);
+}
+
+String findEncrypt(int ssid)
+{
+    switch (WiFi.encryptionType(ssid))
+    {
+    case WIFI_AUTH_OPEN:
+        return "Open";
+    case WIFI_AUTH_WEP:
+        return "WEP";
+    case WIFI_AUTH_WPA_PSK:
+        return "WPA";
+    case WIFI_AUTH_WPA2_PSK:
+        return "WPA2";
+    case WIFI_AUTH_WPA_WPA2_PSK:
+        return "WPA WPA2";
+    case WIFI_AUTH_WPA2_ENTERPRISE:
+        return "Enterprise";
+    default:
+        return "Unknown";
+    }
+}
+
+void loop()
+{
+    // Check if the button is pressed
+    int n = WiFi.scanNetworks();
+    if (n == 0)
+    {
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_profont10_mr);
+        u8g2.drawStr(x, y, "No networks found");
+    }
+    else
+    {
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_profont10_mr);
+        String str = String(n) + " networks found";
+        u8g2.drawStr(x, y, str.c_str());
+        for (int i = 0; i < n; ++i)
+        {
+            // Print SSID and RSSI for each network found
+            String secured = findEncrypt(i);
+            String str = String(i + 1) + ": " + WiFi.SSID(i) + " (" + WiFi.RSSI(i) + ") - " + secured;
+            int y2 = y + 10 + (10 * i);
+            u8g2.drawStr(x, y2, str.c_str());
+            delay(10);
+        }
+    }
+    u8g2.sendBuffer();
+    // Wait a bit before checking the button state again
+    delay(2000);
+}
